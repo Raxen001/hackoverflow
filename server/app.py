931b752227f57a51cbb1efc996a832048f7d6013
Rithesh S <raxen001@tutamail.com>
2023-11-05 04:35:04 +0530

    a

--- server/app.py
+++ server/app.py
@@ -1,15 +1,78 @@
-from flask import Flask, render_template, request, redirect, flash, jsonify
+# importing needed libraries
+import pyotp
+from flask import *
+from flask_bootstrap import Bootstrap
 
+# configuring flask application
 app = Flask(__name__)
-app.secret_key = "secret"
+# change this
+app.config["SECRET_KEY"] = "APP_SECRET_KEY"
+Bootstrap(app)
 
 
-@app.route('/')
-def hello():
-    """Hello world."""
-    return "<h1>hello guys this is awesome</h1>"
+# homepage route
+@app.route("/")
+def index():
+    return "Welcome to the reader's club"
 
 
+@app.route("/profile")
+def profile():
+    return "this is your profile"
+
+
+# 2FA page route
+@app.route("/login")
+def login_2fa():
+    # get secret from db
+    secret = "helloworld"
+    return render_template("login_2fa.html", secret=secret)
+
+
+# 2FA form route
+@app.route("/login", methods=["POST"])
+def login_2fa_form():
+    secret = "helloworld"
+    otp = int(request.form.get("otp"))
+
+    # verifying submitted OTP with PyOTP
+    if pyotp.TOTP(secret).verify(otp):
+        # inform users if OTP is valid
+        flash("The TOTP 2FA token is valid", "success")
+        return redirect(url_for("profile"))
+    else:
+        # inform users if OTP is invalid
+        flash("You have supplied an invalid 2FA token!", "danger")
+        return redirect(url_for("login_2fa"))
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+
+@app.route("/signup", methods=["POST"])
+def signup_form():
+    import mail
+    import qrcode
+
+    email = request.form.get("email")
+    secret = pyotp.random_base32()
+    # change hello world to db
+    totp = pyotp.TOTP("helloworld")
+    account_name = email
+    issuer = "Reader's Club"
+    provisioning_uri = totp.provisioning_uri(
+        name=account_name, issuer_name=issuer)
+
+    img = qrcode.make(provisioning_uri)
+    file_name = "qrcodes/" + email + ".png"
+    img.save(file_name)
+    mail.gmail_send_message(email, file_name, provisioning_uri)
+
+    return redirect(url_for('login_2fa'))
+
+
+# running flask server
 if __name__ == "__main__":
-    # app.run()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(debug=True)
--- server/mail.py
+++ server/mail.py
@@ -1,56 +1,93 @@
 from __future__ import print_function
 
 import base64
+import mimetypes
+import os
 from email.message import EmailMessage
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
 
 import google.auth
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 
 
-def gmail_create_draft():
-    """Create and insert a draft email.
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+
+
+def gmail_send_message(receiver_mail="raxengamer001@gmail.com", img="./icon.png", uri=""):
+    """Create and insert a draft email with attachment.
        Print the returned draft's message and id.
-       Returns: Draft object, including draft id and message meta data.
+      Returns: Draft object, including draft id and message meta data.
 
       Load pre-authorized user credentials from the environment.
       TODO(developer) - See https://developers.google.com/identity
       for guides on implementing OAuth2 for the application.
     """
-    creds, _ = google.auth.default()
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
 
     try:
-        # create gmail api client
         service = build('gmail', 'v1', credentials=creds)
-
         message = EmailMessage()
 
         message.set_content('This is automated draft mail')
 
-        message['To'] = 'gduser1@workspacesamples.dev'
-        message['From'] = 'gduser2@workspacesamples.dev'
-        message['Subject'] = 'Automated draft'
+        message['To'] = receiver_mail
+        message['From'] = '211001084@rajalakshmi.edu.in'
+        message['Subject'] = 'OTP Verification'
+
+        message_content = uri + "User this if the qrcode doesn't work"
+
+        message.set_content(
+            message_content
+        )
+
+        # attachment
+        attachment_filename = img
+        # guessing the MIME type
+        type_subtype, _ = mimetypes.guess_type(attachment_filename)
+        maintype, subtype = type_subtype.split('/')
+
+        with open(attachment_filename, 'rb') as fp:
+            attachment_data = fp.read()
+        message.add_attachment(attachment_data, maintype, subtype)
 
         # encoded message
-        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+            .decode()
 
         create_message = {
-            'message': {
-                'raw': encoded_message
-            }
+            'raw': encoded_message
         }
+
         # pylint: disable=E1101
-        draft = service.users().drafts().create(userId="me",
-                                                body=create_message).execute()
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
 
-        print(F'Draft id: {draft["id"]}\nDraft message: {draft["message"]}')
-
+        print(F'Message Id: {send_message["id"]}')
     except HttpError as error:
         print(F'An error occurred: {error}')
-        draft = None
-
-    return draft
+        send_message = None
+    return send_message
 
 
 if __name__ == '__main__':
-    gmail_create_draft()
+    gmail_send_message()
